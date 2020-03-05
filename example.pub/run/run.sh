4eb66b6521eaa08bdc20bbeeb9238e4a8b383da1
YingZhou001 <31318632+YingZhou001@users.noreply.github.com>
2020-03-05 07:03:48 -0800

    Update run.sh

--- example.pub/run/run.sh
+++ example.pub/run/run.sh
@@ -1,5 +1,17 @@
 map="plink.map"
 ind="ind.txt"
-ibdfile="ibdhead.txt"
+ibdfile="ibd.txt"
 
-./IBDkin -ibdfile ${ibdfile} -map ${map} -ind ${ind} -nthreads 5 -out test --outmask --outcoverage
+#example-1
+./IBDkin -ibdfile ${ibdfile} -map ${map} -ind ${ind} -nthreads 5 -out example-1 --outmask --outcoverage
+
+
+#example-2
+./IBDkin -ibdfile ${ibdfile} -map ${map} -ind ${ind} -nthreads 5 -out example-2 --outmask --outcoverage --nokinship
+
+#example-3
+for part in {1..5}
+do
+  ./IBDkin -ibdfile ${ibdfile} -map ${map} -ind ${ind} -nthreads 5 -out example-3.${part} -part 5 ${part}
+done
+
