535c5cfb76df6b02082d7bd4234b4a561f83d8d8
YingZhou001 <31318632+YingZhou001@users.noreply.github.com>
2020-03-06 09:26:49 -0800

    Update run.sh

--- example.pub/run/run.sh
+++ example.pub/run/run.sh
@@ -10,8 +10,7 @@ ibdfile="ibd.txt"
 ./IBDkin -ibdfile ${ibdfile} -map ${map} -ind ${ind} -nthreads 5 -out example-2 --outmask --outcoverage --nokinship
 
 #example-3
-for part in {1..5}
-do
-  ./IBDkin -ibdfile ${ibdfile} -map ${map} -ind ${ind} -nthreads 5 -out example-3.${part} -part 5 ${part}
-done
+part=1
+./IBDkin -ibdfile ${ibdfile} -map ${map} -ind ${ind} -nthreads 5 -out example-3.${part} -part 5 ${part}
+
 
