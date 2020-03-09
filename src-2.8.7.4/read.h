4385ed92fc0e2c275edc384425cf97cd90650d0f
YingZhou001 <31318632+YingZhou001@users.noreply.github.com>
2020-03-08 17:02:33 -0700

    new update
    1. change "stderr" to "stdout"
    2. support gzip-compressed plink map infput

---
+++ src-2.8.7.4/read.h
@@ -0,0 +1,13 @@
+void read_headfile(char *headfile);
+int check_exist(char *str);
+int check_input(int arg, char**argv);
+void write_relate(void);
+
+void read_map(char *plinkmap);
+void read_ind(char *idfile);
+
+
+void read_ibd_1st_pass(void);
+void read_ibd_2nd_pass(void);
+
+void init(void);
