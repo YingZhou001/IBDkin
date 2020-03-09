8309f54cb34b3b780a8558c8005e1f7e85f748ec
YingZhou001 <31318632+YingZhou001@users.noreply.github.com>
2020-03-08 17:12:17 -0700

    new updates
    1. "stderr" to "stdout"
    2. support gzip-compressed PLINK map input

---
+++ src-v2.8.7.4/print.h
@@ -0,0 +1,9 @@
+void p_mem(void);
+void p_time(void);
+void p_std_time(double seconds);
+void p_maxrss(void);
+
+void p_coverage(void);
+void p_mask(void);
+void p_parameters(void);
+void print_help(void);
