2c216debe6755a09adfb1adf565a8dc7f038534f
YingZhou001 <31318632+YingZhou001@users.noreply.github.com>
2020-10-30 13:17:18 -0700

    Update run.sh

--- example.pub/run/run.sh
+++ example.pub/run/run.sh
@@ -1,6 +1,6 @@
 map="plink.map"
 ind="ind.txt"
-ibdfile="ibdhead.txt"
+ibdfile="ibd.txt"
 range="range.txt"
 
 ./IBDkin --ibdfile ${ibdfile} --map ${map} --ind ${ind} --range ${range} --nthreads 5 --out test --outmask --outcoverage
